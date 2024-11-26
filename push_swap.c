fdf1be2580ab09eb9c23aea9d660c186c4178749
Husrev Akbas <huakbas@c4r3p5.42vienna.com>
2024-11-26 13:30:55 +0100

    args num check changed

--- push_swap.c
+++ push_swap.c
@@ -6,33 +6,57 @@
 /*   By: huakbas <huakbas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 15:26:52 by huakbas           #+#    #+#             */
-/*   Updated: 2024/11/26 13:06:22 by huakbas          ###   ########.fr       */
+/*   Updated: 2024/11/26 13:27:20 by huakbas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(int argc, char **argv)
+int	print_error()
+{
+	return (write(1, "Error\n", 6));
+}
+
+int	is_args_num(char **args)
 {
 	int	i;
 	int	j;
 
-	if (argc < 2)
-		return (0);
 	i = 1;
-	while (argv[i])
+	while (args[i])
 	{
 		j = 0;
-		while (argv[i][j])
+		while (args[i][j])
 		{
-			if (j == 0 && (argv[i][j] != '-' || argv[i][j] != '+'))
+			if (j == 0 && (args[i][j] != '-' || args[i][j] != '+'))
 				j++;
-			if (!ft_isdigit(argv[i][j]))
+			if (!ft_isdigit(args[i][j]))
 				return (0);
 			j++;
 		}
 		i++;
 	}
+	return (1);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+		return (0);
+	if(!is_args_num(argv))
+		return (print_error());
+	//{
+	//	j = 0;
+	//	while (argv[i][j])
+	//	{
+	//		if (j == 0 && (argv[i][j] != '-' || argv[i][j] != '+'))
+	//			j++;
+	//		if (!ft_isdigit(argv[i][j]))
+	//			return (0);
+	//		j++;
+	//	}
+	//	i++;
+	//}
 	printf("work!");
 	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,12 +6,13 @@
 /*   By: huakbas <huakbas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 15:26:46 by huakbas           #+#    #+#             */
-/*   Updated: 2024/11/26 12:59:38 by huakbas          ###   ########.fr       */
+/*   Updated: 2024/11/26 13:15:17 by huakbas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+# include <unistd.h>
 # include <stdio.h> //DELETE THIS LINE
 
 int	ft_isdigit(int c);
