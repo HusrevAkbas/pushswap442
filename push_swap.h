caeb2719968f78bed6aed667c0f098fe61892aea
Husrev Akbas <huakbas@c4r3p5.42vienna.com>
2024-11-26 17:53:40 +0100

    struct added

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: huakbas <huakbas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 15:26:46 by huakbas           #+#    #+#             */
-/*   Updated: 2024/11/26 13:15:17 by huakbas          ###   ########.fr       */
+/*   Updated: 2024/11/26 17:55:08 by huakbas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,17 @@
 # define PUSH_SWAP_H
 # include <unistd.h>
 # include <stdio.h> //DELETE THIS LINE
+
+typedef struct s_node
+{
+	int				data;
+	int				medium;
+	int				average;
+	int				max;
+	int				min;
+	int				count;
+	struct s_node	*next;
+} t_node;
 
 int	ft_isdigit(int c);
 
