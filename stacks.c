4066ddb80c052d562fff474fac5bb4e2a4c716f3
Husrev Akbas <huakbas@c4r3p2.42vienna.com>
2024-12-03 18:42:39 +0100

    rotate a loop fixed

--- stacks.c
+++ stacks.c
@@ -6,7 +6,7 @@
 /*   By: huakbas <huakbas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 15:26:54 by huakbas           #+#    #+#             */
-/*   Updated: 2024/12/03 18:17:29 by huakbas          ###   ########.fr       */
+/*   Updated: 2024/12/03 18:41:53 by huakbas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ int	is_rotate_a(t_pslist *head)
 	if (!head)
 		return (0);
 	greaters(head);
-	if (head->greaters <= size_list(head) * 2 / 3)
+	if (head->greaters <= size_list(head) * 2 / 3 && size_list(head) != 3)
 		return (1);
 	return (0);
 }
